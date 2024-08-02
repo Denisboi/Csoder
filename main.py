a7158fcb92ff65bd26e507146950c5751ce489cd
Deasess <94171325+Denisboi@users.noreply.github.com>
2024-08-02 20:21:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from System.Dumpsc import process_sc, decompress_data
 from System.Decode import decode_sc, cut_sprites
 from pathlib import Path
 
-input_folder = "./In-Compressed"
-output_folder = "./Out-Sprites"
-
 def get_files():
-    all_files = os.listdir(input_folder)
-    if (len(all_files) < 1):
-        Console.error('Input directory is Empty !!!')
+    all_files = os.listdir("./")
+    if (len(all_files) < 3):
+        Console.error('Files not found')
         return
 
     files = []
@@ -36,21 +33,20 @@ def extract_images():
             sc_file_name = sc_file[:-3]
             
             # Creating Sub directory for specified file
-            sc_output_dir = f"{output_folder}/{sc_file_name}"
-            Path(f"{sc_output_dir}/texture").mkdir(parents=True, exist_ok=True)
+            sc_output_dir = f"{sc_file_name}"
 
-            with open(f"{input_folder}/{file}", "rb") as f:
+            with open(f"{file}", "rb") as f:
                 print('')
                 Console.info(f"Processing {file}")
                 # extracting texture
-                images = process_sc(f"{sc_file_name}_tex", f.read(), f"{sc_output_dir}/texture/", True)
+                images = process_sc(f"{sc_file_name}_tex", f.read(), f"{sc_output_dir}", True)
 
             if sc_file not in files:
                 Console.warn(f"{sc_file} not found! Will skip cutting images")
                 continue
             
             Console.info(f"Processing {sc_file}")
-            with open(f"{input_folder}/{sc_file}", "rb") as f:
+            with open(f"{sc_file}", "rb") as f:
                 data = decompress_data(f.read(), sc_file_name)                
                 sprite_globals, sprite_data,sheet_data = decode_sc(data, sc_file_name)
                 cut_sprites(sprite_globals, sprite_data, sheet_data, images, f"{sc_output_dir}")
@@ -60,8 +56,6 @@ def extract_images():
 
 if __name__ == '__main__':
     # Verifying and creating necessary directories
-    [os.system(f'mkdir {i}{nul}') for i in [input_folder, output_folder]]
-    Console.info('Directories successfully initiated!')
-
+    
     # main task
     extract_images()
